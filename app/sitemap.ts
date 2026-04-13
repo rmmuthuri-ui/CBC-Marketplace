5f22af7c6cd040f2b380c341119705e71b4e7ecf
Royford Muthuri <rm.muthuri@gmail.com>
2026-04-13 19:11:04 +0545

    Upgrade sitemap with all pages.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,41 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://cbcmarketplace.co.ke',
-      lastModified: new Date(),
-    },
+  const baseUrl = 'https://cbcmarketplace.co.ke'
+
+  const staticPages = [
+    '',
+    '/subjects/mathematics',
+    '/subjects/science-technology',
+    '/subjects/english',
   ]
+
+  const products = [
+    'acids-bases-and-salts',
+    'atoms-elements-compounds',
+    'chemical-reactions-metals',
+    'circle-parts-of-a-circle',
+    'electrochemistry',
+    'experimental-techniques',
+    'finding-the-nth-term',
+    'laws-of-indices',
+    'organic-chemistry',
+    'periodic-table',
+    'similar-shapes',
+    'states-of-matter',
+    'stoichiometry',
+    'straight-line-graphs'
+  ]
+
+  const staticUrls = staticPages.map((path) => ({
+    url: `${baseUrl}${path}`,
+    lastModified: new Date(),
+  }))
+
+  const productUrls = products.map((slug) => ({
+    url: `${baseUrl}/products/${slug}`,
+    lastModified: new Date(),
+  }))
+
+  return [...staticUrls, ...productUrls]
 }
